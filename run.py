20f98ae5e2c7d924445bec14aea747663d6e9e96
Joe Nilan <suburbanwhiteguy@gmail.com>
2018-02-26 18:14:31 -0500

    remove debug

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import app
 
 #shut up about the secret key
 app.secret_key = "askldjfhaskldjfhlaksjhdfklajshdfkljasghdfjklhvgaskjdfygkasjdfhg"
-app.run(debug=True, host='0.0.0.0')
+app.run()
